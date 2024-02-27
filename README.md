0c9f2a51b2b7772228b4d4498c0c4ba6d5d5509a
Ahmed Elbehairy <elbehairy@Ahmed-Elbehairy>
2024-02-28 01:51:15 +0200

    Documentation done!

--- README.md
+++ README.md
@@ -8,22 +8,104 @@ Just clone the repo by typing this command in your terminal:
 
     git clone https://github.com/ahmed-elbehairy7/askgem
 
-then you will find a file named askgem.config, you have to put you api key there so you can use the application. check google documentation for generating api keys.
+then you will find a file named askgem.config, you have to put you api key there so you can use the application. You can create a new api key by clicking this link and seeing the documentation: https://makersuite.google.com/app/apikey
 
     {
         "key" : "Your api key"
     }
 
+Now you need to install the requirements by typing the following command:
+
+    pip install -U -r requirements.txt
+
+> **NOTE:** I had some problems at this step, hope it goes well with you but I did a lot of troubleshooting here because the google generative ai package wasn't enough, sorry that I don't know what happened exactly so I can't document it
+
+### Windows
+
+first install the pyinstaller python package with the following command
+
+    pip install --upgrade pyinstaller
+
+then run the following command
+
+    pyinstaller main.py -F -n askgem
+
+So basicly this is converting the main.py file to an executable file. the "-F" argument states that the program is just one file, and the "-n" for naming the .exe application. so after running this command you should see two new directories, build and dest, open the dest directory and you will find askgem.exe there, congratulations, it can't find the askgem.config file! [see this](#c---config-path-path)
+
+#### access the app from anywhere
+
+In order to do this you had to add a new environment variable, you can search online for how to do this for more updated info or do the following:
+simply press the windows key and start typing environment variables then click it when it appears, after that go environment variables.
+
+Now, you have users variables and system variables. you will find a path named Path, select it and click edit. Now, choose whether you would add a path for only this application or add a path for a directory that will contain any other projects in the future. for me I choose the directory option, then I put all my executables there. So, click new then type the path to the app or the directory whatever you want, this can require a restart I don't know honestly
+
+### Linux
+
+Getting the app to work is literaly as easy as copying a file since what you have to do is just copy the file to the /usr/bin directory
+
+    cp main.py /usr/bin/askgem
+
+## askgem.config
+
+> If your program is not working because it can't find the askgem.config file, [see this](#c---config-path-path)
+
+### key
+
+as obvious, this is the api key for the app, and this is probably the only required one
+
+### model
+
+you can change the model the app uses by changing the model value
+
+### you
+
+this change what the application print to take input from you, for example changing the you to "you: " like this:
+
+    {...
+
+        "you" : "You: "
+
+    ...}
+
+will result of this:
+
+    hostname@uesrname:~$ askgem
+    You: Hello
+
+    hello, there!
+
+    You: exit
+
+instead of this:
+
+    hostname@uesrname:~$ askgem
+    >>> Hello
+
+    hello, there!
+
+    >>> exit
+
+### gemeni
+
+This is pretty much the same as [you](#you) but for gemeni
+
+### youInFile and gemeniInFile
+
+Sometimes files are unreadable, so this one is for how to save prompts and responses in files. They are pretty much the same as [you](#you) and [gemeni](#gemeni) but for saved files, they're defaulted to >>> for you and <<< for gemeni or the you and gemeni values if they're set.
+for saving chat into a file see [save-file](#s---save-file)
+
 ## arguments
 
 ### -c, --config-path path
 
-specify a path for askgem.config file, the config path is set to ./askgem.config as default
+specify a path for askgem.config file, the config path is set to ./askgem.config as default see the example below
+
+    python main.py -c D:\new\path\for\askgem.config
 
 ### -q, --question [ ...]
 
-Ask gemeni one question from the command line directly before starting the app.
-then gemeni will answer this question before anything, see the example below:
+This is probably done with the [-n, --not-interactive](#n---not-interactive) argument, or simply -qn for combining them together. And it's job is asking gemeni one question from the command line directly before starting the app.
+then gemeni will answer this question before anything. see the example below:
 
     hostname@uesrname:~$ askgem -q hello gem
     Hello! I'm here to help you with any questions or tasks you may have. Let me know how I can assist you.
@@ -33,3 +115,36 @@ then gemeni will answer this question before anything, see the example below:
     I am a virtual digital voice service, not a person.
 
     >>> exit
+
+### -n, --non-interactive
+
+This basicly tells the program to not initiate an interactive chat session and close after the first request like the example below:
+
+    hostname@uesrname:~$ askgem -n
+    >>> What is your name
+
+    I am a virtual digital voice service, not a person.
+    hostname@uesrname:~$
+
+### -qn, --question-non-interactive
+
+This is for times when you just want to ask one simple question. See the exaple below for more understanding:
+
+    hostname@uesrname:~$ askgem -qn What is your name
+    I am a virtual digital voice service, not a person.
+    hostname@uesrname:~$
+
+### -s, --save-file
+
+save the chat into a file
+
+    askgem -s file.txt
+
+### -l, --list-models
+
+List gemeni models available, this just print the genai.list_models()
+
+    hostname@uesrname:~$ askgem -l
+    gemeni-pro
+    gemeni-pro-vision
+    ...etc.
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from json import load
 from os import path
 from re import sub
 
+
 CONFIG_PATH = "./askgem.config"
 
 try:
@@ -21,37 +22,43 @@ def main():
     global chat, args
 
     args = parse_args()
-
-    genai.configure(api_key=CONFIG["key"])
-
-    model = genai.GenerativeModel(CONFIG.get("model", "gemini-1.0-pro-latest"))
-
-    chat = model.start_chat(history=[])
-
-    actions = {None: askgem, "list": list_models, "config": config}
+    
+    if not args.action == "config":
+        setup_model()
+        
+    actions = {
+        None: askgem, 
+        "list": list_models, 
+        "config": config
+    }
+        
     return actions[args.action]()
 
 
 def askgem():
+    you = CONFIG.get("you", ">>> ")
+    gemeni = CONFIG.get("gemeni", "")
+
+    if args.save_file:
+        youInFile = CONFIG.get("youInFile", CONFIG.get("you", ">>> "))
+        gemeniInFile = CONFIG.get("gemeniInFile", CONFIG.get("gemeni", "<<< "))
     while True:
         if args.question:
             text = " ".join(args.question)
             args.question = None
         else:
-            text = input(CONFIG.get("you", ">>> "))
+            text = input(you)
             print()
         if text.lower().strip() == CONFIG.get("exit", "exit"):
             return
         response = to_markdown(chat.send_message(text).text).data
-        print(CONFIG.get("gemeni", ""), response)
+        print(gemeni, response)
         if args.save_file:
             with open(args.save_file, "a") as f:
-                f.write(
-                    f"{CONFIG.get('youInFile', '>>>')} {text}\n\n{CONFIG.get('gemeniInFile', '<<<')} {response}\n\n"
-                )
+                f.write(f"{youInFile} {text}\n\n{gemeniInFile} {response}\n\n")
         print("")
 
-        if args.not_interactive:
+        if args.non_interactive:
             return
 
 
@@ -61,13 +68,21 @@ def list_models():
 
 
 def config():
-    with open(path.abspath(__file__), "r+") as file:
-        file.write(
-            sub(
-                'CONFIG_PATH = ".+"', f'CONFIG_PATH = "{args.config_path}"', file.read()
-            )
-        )
+    file = path.abspath(__file__)
+    with open(file, "r") as f:
+        new = sub('\nCONFIG_PATH = ".+"\n', f'\nCONFIG_PATH = "{args.config_path}"\n', f.read(), 1)
+        
+    with open(file, 'w') as f:
+        f.write(new)
 
+
+def setup_model():
+    global chat
+    genai.configure(api_key=CONFIG["key"])
+    
+    model = genai.GenerativeModel(CONFIG.get("model", "gemini-1.0-pro-latest"))
+
+    chat = model.start_chat(history=[])
 
 def parse_args():
     parser = ArgumentParser(description="Ask gemeni from the command line")
@@ -81,7 +96,7 @@ def parse_args():
     )
     parser.add_argument(
         "-n",
-        "--not-interactive",
+        "--non-interactive",
         action="store_true",
         help="Don't initiate an interactive chat session and close after the first request",
     )
@@ -101,128 +116,16 @@ def parse_args():
         "--config-path",
         metavar="",
         help="set the path for the config file",
-        action=ConfigAction,
-    )
-
-    return parser.parse_args()
-
-
-def to_markdown(text):
-    text = text.replace("•", "  *")
-    return Markdown(indent(text, "", predicate=lambda _: True))
-
-
-class ConfigAction(Action):
-    def __call__(self, parser, namespace, values, option_string=None):
-        namespace.action = "config"
-        namespace.config_path = values
-
-
-if __name__ == "__main__":
-    try:
-        main()
-    except KeyboardInterrupt:
-        exit("\n")
-#!/usr/bin/python3
-from argparse import ArgumentParser, Action
-import google.generativeai as genai
-from IPython.display import Markdown
-from textwrap import indent
-from json import load
-from os import path
-from re import sub
-
-CONFIG_PATH = "path"
-
-try:
-    CONFIG = dict(load(open(CONFIG_PATH)))
-except FileNotFoundError:
-    pass
-
-args, chat = [None for _ in range(2)]
-
-
-def main():
-    global chat, args
-
-    args = parse_args()
-
-    genai.configure(api_key=CONFIG["key"])
-
-    model = genai.GenerativeModel(CONFIG.get("model", "gemini-1.0-pro-latest"))
-
-    chat = model.start_chat(history=[])
-
-    actions = {None: askgem, "list": list_models, "config": config}
-    return actions[args.action]()
-
-
-def askgem():
-    while True:
-        if args.question:
-            text = " ".join(args.question)
-            args.question = None
-        else:
-            text = input(CONFIG.get("you", ">>> "))
-            print()
-        if text.lower().strip() == CONFIG.get("exit", "exit"):
-            return
-        response = to_markdown(chat.send_message(text).text).data
-        print(CONFIG.get("gemeni", ""), response)
-        if args.save_file:
-            with open(args.save_file, "a") as f:
-                f.write(
-                    f"{CONFIG.get('youInFile', '>>>')} {text}\n\n{CONFIG.get('gemeniInFile', '<<<')} {response}\n\n"
-                )
-        print("")
-
-        if args.not_interactive:
-            return
-
-
-def list_models():
-    for model in genai.list_models():
-        print(model.name)
-
-
-def config():
-    with open(path.abspath(__file__), "r+") as file:
-        file.write(sub('CONFIG_PATH = "path"', file.read()))
-
-
-def parse_args():
-    parser = ArgumentParser(description="Ask gemeni from the command line")
-    parser.add_argument(
-        "-q",
-        "--question",
-        help="Ask gemeni one question from command line directly",
-        type=str,
-        nargs="+",
-        metavar="",
-    )
-    parser.add_argument(
-        "-n",
-        "--not-interactive",
-        action="store_true",
-        help="Don't initiate an interactive chat session and close after the first request",
-    )
-    parser.add_argument(
-        "-s", "--save-file", metavar="", help="Save the chat history to a file"
-    )
-    parser.add_argument(
-        "-l",
-        "--list-models",
-        help="List gemeni models there",
-        const="list",
-        action="store_const",
         dest="action",
+        action=ConfigAction,
     )
     parser.add_argument(
-        "-c",
-        "--config-path",
-        metavar="",
-        help="set the path for the config file",
-        action=ConfigAction,
+        "-qn",
+        "--question-non-interactive",
+        help="-questoin and --non-interactive at the same time",
+        metavar='',
+        action=qnAction,
+        nargs='+'
     )
 
     return parser.parse_args()
@@ -235,250 +138,13 @@ def to_markdown(text):
 
 class ConfigAction(Action):
     def __call__(self, parser, namespace, values, option_string=None):
-        namespace.action = "config"
+        setattr(namespace, self.dest, "config")
         namespace.config_path = values
 
-
-if __name__ == "__main__":
-    try:
-        main()
-    except KeyboardInterrupt:
-        exit("\n")
-#!/usr/bin/python3
-from argparse import ArgumentParser, Action
-import google.generativeai as genai
-from IPython.display import Markdown
-from textwrap import indent
-from json import load
-from os import path
-from re import sub
-
-CONFIG_PATH = "path"
-
-try:
-    CONFIG = dict(load(open(CONFIG_PATH)))
-except FileNotFoundError:
-    pass
-
-args, chat = [None for _ in range(2)]
-
-
-def main():
-    global chat, args
-
-    args = parse_args()
-
-    genai.configure(api_key=CONFIG["key"])
-
-    model = genai.GenerativeModel(CONFIG.get("model", "gemini-1.0-pro-latest"))
-
-    chat = model.start_chat(history=[])
-
-    actions = {None: askgem, "list": list_models, "config": config}
-    return actions[args.action]()
-
-
-def askgem():
-    while True:
-        if args.question:
-            text = " ".join(args.question)
-            args.question = None
-        else:
-            text = input(CONFIG.get("you", ">>> "))
-            print()
-        if text.lower().strip() == CONFIG.get("exit", "exit"):
-            return
-        response = to_markdown(chat.send_message(text).text).data
-        print(CONFIG.get("gemeni", ""), response)
-        if args.save_file:
-            with open(args.save_file, "a") as f:
-                f.write(
-                    f"{CONFIG.get('youInFile', '>>>')} {text}\n\n{CONFIG.get('gemeniInFile', '<<<')} {response}\n\n"
-                )
-        print("")
-
-        if args.not_interactive:
-            return
-
-
-def list_models():
-    for model in genai.list_models():
-        print(model.name)
-
-
-def config():
-    with open(path.abspath(__file__), "r+") as file:
-        file.write(sub('CONFIG_PATH = "path"', file.read()))
-
-
-def parse_args():
-    parser = ArgumentParser(description="Ask gemeni from the command line")
-    parser.add_argument(
-        "-q",
-        "--question",
-        help="Ask gemeni one question from command line directly",
-        type=str,
-        nargs="+",
-        metavar="",
-    )
-    parser.add_argument(
-        "-n",
-        "--not-interactive",
-        action="store_true",
-        help="Don't initiate an interactive chat session and close after the first request",
-    )
-    parser.add_argument(
-        "-s", "--save-file", metavar="", help="Save the chat history to a file"
-    )
-    parser.add_argument(
-        "-l",
-        "--list-models",
-        help="List gemeni models there",
-        const="list",
-        action="store_const",
-        dest="action",
-    )
-    parser.add_argument(
-        "-c",
-        "--config-path",
-        metavar="",
-        help="set the path for the config file",
-        action=ConfigAction,
-    )
-
-    return parser.parse_args()
-
-
-def to_markdown(text):
-    text = text.replace("•", "  *")
-    return Markdown(indent(text, "", predicate=lambda _: True))
-
-
-class ConfigAction(Action):
+class qnAction(Action):
     def __call__(self, parser, namespace, values, option_string=None):
-        namespace.action = "config"
-        namespace.config_path = values
-
-
-if __name__ == "__main__":
-    try:
-        main()
-    except KeyboardInterrupt:
-        exit("\n")
-#!/usr/bin/python3
-from argparse import ArgumentParser, Action
-import google.generativeai as genai
-from IPython.display import Markdown
-from textwrap import indent
-from json import load
-from os import path
-from re import sub
-
-CONFIG_PATH = "path"
-
-try:
-    CONFIG = dict(load(open(CONFIG_PATH)))
-except FileNotFoundError:
-    pass
-
-args, chat = [None for _ in range(2)]
-
-
-def main():
-    global chat, args
-
-    args = parse_args()
-
-    genai.configure(api_key=CONFIG["key"])
-
-    model = genai.GenerativeModel(CONFIG.get("model", "gemini-1.0-pro-latest"))
-
-    chat = model.start_chat(history=[])
-
-    actions = {None: askgem, "list": list_models, "config": config}
-    return actions[args.action]()
-
-
-def askgem():
-    while True:
-        if args.question:
-            text = " ".join(args.question)
-            args.question = None
-        else:
-            text = input(CONFIG.get("you", ">>> "))
-            print()
-        if text.lower().strip() == CONFIG.get("exit", "exit"):
-            return
-        response = to_markdown(chat.send_message(text).text).data
-        print(CONFIG.get("gemeni", ""), response)
-        if args.save_file:
-            with open(args.save_file, "a") as f:
-                f.write(
-                    f"{CONFIG.get('youInFile', '>>>')} {text}\n\n{CONFIG.get('gemeniInFile', '<<<')} {response}\n\n"
-                )
-        print("")
-
-        if args.not_interactive:
-            return
-
-
-def list_models():
-    for model in genai.list_models():
-        print(model.name)
-
-
-def config():
-    with open(path.abspath(__file__), "r+") as file:
-        file.write(sub('CONFIG_PATH = "path"', file.read()))
-
-
-def parse_args():
-    parser = ArgumentParser(description="Ask gemeni from the command line")
-    parser.add_argument(
-        "-q",
-        "--question",
-        help="Ask gemeni one question from command line directly",
-        type=str,
-        nargs="+",
-        metavar="",
-    )
-    parser.add_argument(
-        "-n",
-        "--not-interactive",
-        action="store_true",
-        help="Don't initiate an interactive chat session and close after the first request",
-    )
-    parser.add_argument(
-        "-s", "--save-file", metavar="", help="Save the chat history to a file"
-    )
-    parser.add_argument(
-        "-l",
-        "--list-models",
-        help="List gemeni models there",
-        const="list",
-        action="store_const",
-        dest="action",
-    )
-    parser.add_argument(
-        "-c",
-        "--config-path",
-        metavar="",
-        help="set the path for the config file",
-        action=ConfigAction,
-    )
-
-    return parser.parse_args()
-
-
-def to_markdown(text):
-    text = text.replace("•", "  *")
-    return Markdown(indent(text, "", predicate=lambda _: True))
-
-
-class ConfigAction(Action):
-    def __call__(self, parser, namespace, values, option_string=None):
-        namespace.action = "config"
-        namespace.config_path = values
+        namespace.question = str(values)
+        namespace.non_interactive = True
 
 
 if __name__ == "__main__":
